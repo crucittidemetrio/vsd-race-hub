a48f4374113e12b1bc6601482b5f2e908bbfcd6c
Demetrio <crucitti.dem@gmail.com>
2026-04-23 13:33:26 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@
 
 // L'URL del tuo Google Apps Script (lo ottieni dopo il deploy)
 // Esempio: "https://script.google.com/macros/s/AKfycbx.../exec"
-export const API_URL = "INCOLLA_QUI_IL_TUO_URL_APPS_SCRIPT";
+export const API_URL = "https://script.google.com/macros/s/AKfycbxBhzMK9l3fPN3j7bDtRxpXAp_jON0IsKIzI_8IKjiZrUijS92jdbSLUtozh7g5aQLkvQ/exec";
 
 // La password per lo staff (deve corrispondere a quella nel Apps Script)
 export const STAFF_PASSWORD = "VSD2025staff";
